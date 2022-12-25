1f9ce9a42f570f2e34be34552cf10319b0f2bbed
Coca-Sprite <76017078+Coca-Sprite@users.noreply.github.com>
2022-12-25 22:44:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import webbrowser
-import time
-import keyboard
+#import一些東西
+import webbrowser #import webbrowser
+import time #import time
+import keyboard #import keyboard
 
-times = 0
+#變數設定
+times = 0 #時間的變數
+urL='https://nafstore.net/account' #設定開啟的網址
 
-while True:
-	times += 1
-	time.sleep(1)
-	if times == 28800:
-		urL='https://discord.com/channels/991097038382964777/1007154010085662721'
-		webbrowser.get('windows-default').open_new(urL)
-		time.sleep(15)
-		keyboard.write("!s")
-		keyboard.press_and_release("enter")
-		times = 0
-	print (f"{times}")
+#不斷重複
+while True: #就是我讓這裡不會停的
+	times += 1 #把times +1
+	time.sleep(1) #等一秒Code也是會累的
+	if times == 86410: #如果時間過了24小時+10秒(避免與網站計時不同)
+		webbrowser.get('windows-default').open_new(urL) #開啟https://nafstore.net/account
+		times = 0 #重製時間
+	print (f"{times}") #顯示現在經過幾秒
